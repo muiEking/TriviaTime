1cf37b1abf6639759bd395b62cc7f6ee380adc4c
Elisha Thompson <elishathompson62@gmail.com>
2024-04-13 15:39:25 +0000

    Firebase setup completed and deployed

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBd5zji0Kd04frEvXYWR9dvIiMozMCC6q4",
+    authDomain: "triviatime-1c31d.firebaseapp.com",
+    projectId: "triviatime-1c31d",
+    storageBucket: "triviatime-1c31d.appspot.com",
+    messagingSenderId: "385717513256",
+    appId: "1:385717513256:web:b62e51b5f160a11050191d",
+    measurementId: "G-33GMW5QP64"
+  };
+
+  export default firebaseConfig
