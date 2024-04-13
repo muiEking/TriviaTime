563310aad87fb7b5d911efa22263f39e661e1046
ekingsservices <31592170-ekingsservices@users.noreply.replit.com>
2024-04-13 13:30:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+let score = 0;
+
+function fetchTrivia() {
+  fetch('https://opentdb.com/api.php?amount=1&type=multiple')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      displayTrivia(data.results[0]);
+    })
+    .catch(error => {
+      console.error('Fetching trivia failed:', error);
+      fetchreload();
+    });
+}
+
+function displayTrivia(data) {
+  const questionElement = document.getElementById('trivia');
+  const answersElement = document.getElementById('answers');
+
+  questionElement.innerHTML = data.question;
+  answersElement.innerHTML = '';
+
+  const answers = [...data.incorrect_answers];
+  const correctAnswerIndex = Math.floor(Math.random() * (answers.length + 1));
+  answers.splice(correctAnswerIndex, 0, data.correct_answer);
+
+  answers.forEach(answer => {
+    const li = document.createElement('li');
+    li.classList.add('answer');
+    li.innerHTML = answer;
+    li.addEventListener('click', () => {
+      if (answer === data.correct_answer) {
+        li.classList.add('correct');
+        score++;
+        document.getElementById('score').innerText = 'Score: ' + score;
+      } else {
+        li.classList.add('wrong');
+      }
+      fetchTrivia(); // Fetch new trivia after answering
+    });
+    answersElement.appendChild(li);
+  });
+}
+
+function fetchreload() {
+  fetchTrivia();
+}
+
+fetchTrivia();
